067461c6896d9a95791a91b38e83fc4f81e22ec3
Chris Mayo <cmayo@microsoft.com>
2017-02-14 20:10:07 -0800

    Added Topic 1 and subtopic dialogs.

--- NavigationBot/Dialogs/Topic1Dialog.cs
+++ NavigationBot/Dialogs/Topic1Dialog.cs
@@ -11,7 +11,9 @@ namespace NavigationBot.Dialogs
     [Serializable]
     public class Topic1Dialog : IDialog<object>
     {
-        private string[] subtopics = { "Topic 1.1", "Topic 1.2", "Topic 1.3" };
+        private const string Topic1_1Option = "Topic 1.1";
+        private const string Topic1_2Option = "Topic 1.2";
+        private const string Topic1_3Option = "Topic 1.3";
 
         public async Task StartAsync(IDialogContext context)
         {
@@ -24,36 +26,69 @@ namespace NavigationBot.Dialogs
 
             reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
             
-            for (int i = 0; i < subtopics.Length; i++)
-            {
-                var groupHeroCard = new HeroCard
-                {
-                    Title = subtopics[i],
-                    Text = $"To learn more about { subtopics[i] }, click below...",
-
-                    Images = new List<CardImage>() {
-                        new CardImage($"https://placeholdit.imgix.net/~text?txtsize=28&txt={ subtopics[i] }%201&w=320&h=160") },
-
-                    Buttons = new List<CardAction>
-                    {
-                        new CardAction(ActionTypes.ImBack, subtopics[i], value: subtopics[i])
-                    }
-                };
-
-                reply.Attachments.Add(groupHeroCard.ToAttachment());
-            }
+            reply.Attachments.Add(CreateHeroCardAttachment(Topic1_1Option));
+            reply.Attachments.Add(CreateHeroCardAttachment(Topic1_2Option));
+            reply.Attachments.Add(CreateHeroCardAttachment(Topic1_3Option));
 
             await context.PostAsync(reply);
 
             context.Wait(this.ShowMenuResumeAfterAsync);
         }
 
+        private Attachment CreateHeroCardAttachment(string title)
+        {
+            var heroCard = new HeroCard
+            {
+                Title = title,
+                Text = $"To learn more about { title }...",
+
+                Images = new List<CardImage>() {
+                        new CardImage($"https://placeholdit.imgix.net/~text?txtsize=28&txt={ HttpContext.Current.Server.UrlEncode(title) }&w=320&h=160") },
+
+                Buttons = new List<CardAction>
+                {
+                    new CardAction(ActionTypes.ImBack, title, value: title)
+                }
+            };
+
+            return heroCard.ToAttachment();
+        }
+
         private async Task ShowMenuResumeAfterAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
             var message = await result;
-            // Start here: Wire up retries. Wire up launching sub dialogs with "more".
 
-            await this.StartOverAsync(context, $"'{ message.Text }' isn't ready yet.");
+            if (message.Text == Topic1_1Option)
+            {
+                context.Call(new Topic1_1Dialog(), Topic1_1DialogResumeAfterAsync);
+            }
+            else if (message.Text == Topic1_2Option)
+            {
+                context.Call(new Topic1_2Dialog(), Topic1_2DialogResumeAfterAsync);
+            }
+            else if (message.Text == Topic1_3Option)
+            {
+                context.Call(new Topic1_3Dialog(), Topic1_3DialogResumeAfterAsync);
+            }
+            else
+            {
+                await this.StartOverAsync(context, $"I'm sorry, I don't understand '{ message.Text }'.");
+            }
+        }
+
+        private async Task Topic1_3DialogResumeAfterAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            await this.ShowMenuAsync(context);
+        }
+
+        private async Task Topic1_2DialogResumeAfterAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            await this.ShowMenuAsync(context);
+        }
+
+        private async Task Topic1_1DialogResumeAfterAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            await ShowMenuAsync(context);
         }
 
         private async Task StartOverAsync(IDialogContext context, string text)
--- NavigationBot/Dialogs/Topic1_2Dialog.cs
+++ NavigationBot/Dialogs/Topic1_2Dialog.cs
@@ -1,11 +1,92 @@
-﻿using System;
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Threading.Tasks;
 
 namespace NavigationBot.Dialogs
 {
-    public class Topic1_2Dialog
+    [Serializable]
+    public class Topic1_2Dialog : IDialog<object>
     {
+        private const string more = "more";
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            PromptDialog.Choice(context, FirstPromptResumeAfter, new[] { more }, "Topic 1.2 Dialog dialog text...",
+                "I'm sorry, I don't understand. Please try again.");
+        }
+
+        private async Task FirstPromptResumeAfter(IDialogContext context, IAwaitable<string> result)
+        {
+            try
+            {
+                var choice = await result;
+
+                if (choice == more)
+                {
+                    PromptDialog.Choice(context, SecondPromptResumeAfter, new[] { more }, "Topic 1.2 Dialog second dialog text...",
+                        "I'm sorry, I don't understand. Please try again.");
+                }
+            }
+            catch (TooManyAttemptsException)
+            {
+                context.Fail(new TooManyAttemptsException("Too many attempts."));
+            }
+        }
+
+        private async Task SecondPromptResumeAfter(IDialogContext context, IAwaitable<string> result)
+        {
+            try
+            {
+                var choice = await result;
+
+                if (choice == more)
+                {
+                    PromptDialog.Choice(context, ThirdPromptResumeAfter, new[] { more }, "Topic 1.2 Dialog third dialog text...",
+                        "I'm sorry, I don't understand. Please try again.");
+                }
+            }
+            catch (TooManyAttemptsException)
+            {
+                context.Fail(new TooManyAttemptsException("Too many attempts."));
+            }
+        }
+
+        private async Task ThirdPromptResumeAfter(IDialogContext context, IAwaitable<string> result)
+        {
+            try
+            {
+                var choice = await result;
+
+                if (choice == more)
+                {
+                    PromptDialog.Choice(context, FourthPromptResumeAfter, new[] { "Menu", "Topic 1" }, "Topic 1.2 Dialog is done. What do you want to do next?...",
+                        "I'm sorry, I don't understand. Please try again.");
+                }
+            }
+            catch (TooManyAttemptsException)
+            {
+                context.Fail(new TooManyAttemptsException("Too many attempts."));
+            }
+        }
+
+        private async Task FourthPromptResumeAfter(IDialogContext context, IAwaitable<string> result)
+        {
+            try
+            {
+                var choice = await result;
+
+                if (choice == "Menu" || choice == "Topic 1")
+                {
+                    context.Done<object>(null);
+                }
+            }
+            catch (TooManyAttemptsException)
+            {
+                context.Fail(new TooManyAttemptsException("Too many attempts."));
+            }
+        }
     }
 }
